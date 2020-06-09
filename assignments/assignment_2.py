28184affc4d751613fe7fb068380532f69413f7b
Arianna <sarozadr@gmail.com>
2020-06-09 01:31:32 -0400

    Added comments and deleted unnecessary code

--- assignments/assignment_2.py
+++ assignments/assignment_2.py
@@ -2,7 +2,6 @@
 Created on May 15, 2020
 """
 
-
 """Collection Assignment
 1. Create a list of countries
 2. Initialize the list with three countries
@@ -12,62 +11,46 @@ Created on May 15, 2020
 6. Repeat operations with a set"""
 
 
-
-#create a list
-countries_list = ["Cuba", "US","Canada"]
+#Creates and initializes a list of countries.
+countries_list = ["Cuba","US","Canada"]
 print(countries_list)
 
-
-#add a country
+#Adds a country to this list.
 countries_list.append("Peru")
 print(countries_list)
 
-
-#remove by value another way
-#countries_list.remove("Cuba")
-#print(countries_list)
-
-#remove by index, another way
-#del countries_list[0]
-#print(countries_list)
-
-#remove at the position 
-countries_list.pop(0)
+#Removes country by index.
+del countries_list[1]
 print(countries_list)
 
-
-#length for the middle of the list
+#Calculates length of this country list divided by two.
 countries_list_len = len(countries_list)
 countries_list_len = int(countries_list_len /2)
 
-#add in the middle of the list
+#Adds a country in the middle of this list.
 countries_list.insert( countries_list_len, "Mexico")
 print(countries_list)
 
-
-
-
-
-#create set
+#Creates and initializes a set.
 countries_set = {"Brazil","Argentina", "France"}
 print(countries_set)
-#add a country to the set
+
+#Adds a country to this set.
 countries_set.add("Peru")
 print(countries_set)
 
-#create a list and add the set to the list 
-#to be able to perform more operations
+#Casts the set to a list to perform more operations.
 list_of_countries = list(countries_set)
 
-#eliminate the first element in the list
-list_of_countries.pop(0)
+#Removes country by index.
+del list_of_countries[1]
 print(list_of_countries)
 
-#length for the middle of the list
+#Calculates length of this country list divided by two.
 countries_len = len(list_of_countries)
 countries_len = int(countries_len/2)
 
-#insert an element in the middle of the list
+#Inserts an element in the middle of this list and casts this list to a set.
 list_of_countries.insert(countries_len, "Italy")
 print(set(list_of_countries))
 
