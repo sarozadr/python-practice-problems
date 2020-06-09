b3b380468e74a8d401358b9eeeeb14886f04d4f6
Arianna <sarozadr@gmail.com>
2020-06-09 00:26:52 -0400

    Added description

--- assignments/assignment_1.py
+++ assignments/assignment_1.py
@@ -2,6 +2,14 @@
 Created on May 15, 2020
 """
 
+"""Define four different variables
+1. First variable integer type
+2. Second variable decimal type
+3. Third variable boolean type
+4. Fourth variable string type
+Print each value to the console"""
+
+
 num = 10
 float_num = 20.54
 varieble = True
