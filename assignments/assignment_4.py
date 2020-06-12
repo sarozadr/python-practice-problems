438103394db6f5bee84584c8597e1487759b3124
Arianna <sarozadr@gmail.com>
2020-06-12 14:51:37 -0400

    Added comments

--- assignments/assignment_4.py
+++ assignments/assignment_4.py
@@ -9,10 +9,11 @@ Display all the numbers up to that number
 Skip the multiples of 10 (used continuous)
 Stop is the number is greater that 100 (break)"""
 
-
+#Declares variables and receives input and casts the input to an integer.
 num1 = int(input("Please enter a number:"))
 num2 = 0
 
+#Checks conditions
 while num2 != num1 and num1 <= 100:
     num2 = num2 +1
     if num2 % 10 != 0:
