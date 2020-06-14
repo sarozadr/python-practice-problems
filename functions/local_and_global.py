84c8799aca8c5f5ed098d25912266a94407c26de
Arianna <sarozadr@gmail.com>
2020-06-14 12:56:58 -0400

    Added comments

--- functions/local_and_global.py
+++ functions/local_and_global.py
@@ -6,8 +6,13 @@ Created on July 14, 2020
 #This is a Global Variable.
 #Can be access any where.
 x = 123
-
 print(x)
 
 def display():
+    #Declares local variable.
+    y = 456
     print(x)
+
+
+#This variable value can not be access, because it inside the  display function.
+#print(y)
