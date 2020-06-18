22eee9dc5fead16d81c531ea159e9cdab137b48f
Arianna <sarozadr@gmail.com>
2020-06-18 18:09:08 -0400

    Initial commit

---
+++ functions/cube_lambda.py
@@ -0,0 +1,22 @@
+"""Author Arianna Delgado
+Created on July 18, 2020
+"""
+
+""" Lambdas"""
+
+""" Anonymous Functions (no name)."""
+"""Lambdas argument: logic expression --> return a function"""
+
+"""Create a lambda that will calculate the cube of a given number"""
+
+f = lambda num: num ** 3
+
+print(f(2))
+
+
+
+
+
+
+
+
