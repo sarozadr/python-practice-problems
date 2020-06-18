b1f910653fefa93b455f7a8b2892ac1fd1fb95ae
Arianna <sarozadr@gmail.com>
2020-06-18 17:19:50 -0400

    Fixed Typo

---
+++ functions/defual_arguments.py
@@ -0,0 +1,10 @@
+"""Author Arianna Delgado
+Created on July 14, 2020
+"""
+#Defines a function to calculate average.
+def average(a = 20 ,b = 10):
+    #This variables get values as we send it.
+    #The order is not important the keywords are important for the values.
+    print(a)
+    print(b)
+    return (a+b)/2
