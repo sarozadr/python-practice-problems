004cf14b6a696f018423e452293b16b57331bbf9
Arianna <sarozadr@gmail.com>
2020-06-18 17:43:04 -0400

    Renamed File

---
+++ functions/default_arguments.py
@@ -0,0 +1,14 @@
+"""Author Arianna Delgado
+Created on July 14, 2020
+"""
+
+#Defines a function to calculate average.
+#Values a and b are by default 20 and 10.
+def average(a = 20 ,b = 10):
+    print(a)
+    print(b)
+    return (a+b)/2
+
+
+#Changes the default value add arguments to the function.
+print(average(a = 50))    
