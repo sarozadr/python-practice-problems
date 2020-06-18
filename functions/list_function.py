61f69070a0d348675e6bf1d20cff91af05c75f45
Arianna <sarozadr@gmail.com>
2020-06-18 16:39:47 -0400

    Initial commit

---
+++ functions/list_function.py
@@ -0,0 +1,11 @@
+"""Author Arianna Delgado
+Created on July 18, 2020
+"""
+
+
+#This function fun() displays a list of numbers one by one
+def fun(list_num):
+    for i in list_of:
+        print (i)
+
+fun([1,2,3,4,5,6])
