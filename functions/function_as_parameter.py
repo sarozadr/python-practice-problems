740dc54b2615026cdf109f044bfc0d395cbc0570
Arianna <sarozadr@gmail.com>
2020-06-18 15:58:12 -0400

    Initial commit

---
+++ functions/function_as_parameter.py
@@ -0,0 +1,14 @@
+"""Author Arianna Delgado
+Created on July 18, 2020
+"""
+
+
+#Pass a a function as a parameter to the function.
+def display(fun):
+    return "Hello " + fun
+
+def name():
+    return "Arianna"
+
+#Calls the name() function and the results is pass to display() function and print the result.
+print(display(name()))
