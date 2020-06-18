f178e9dcfa6689a23e7d55e20186a9f233704061
Arianna <sarozadr@gmail.com>
2020-06-18 16:33:57 -0400

    Initital commit

---
+++ functions/return_function.py
@@ -0,0 +1,18 @@
+"""Author Arianna Delgado
+Created on July 18, 2020
+"""
+
+#Defines a function inside another function.
+def display():
+    #This function message() will only be ablalible to access inside the function display().
+    def message():
+        return "Hello"
+    return message
+
+
+fun = display()
+print(fun())
+  
+
+
+
