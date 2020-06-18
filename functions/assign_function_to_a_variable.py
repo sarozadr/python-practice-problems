e0f7842a806c071eab12f8397d54b588b71a0a91
Arianna <sarozadr@gmail.com>
2020-06-18 15:00:54 -0400

    Initial commit

---
+++ functions/assign_function_to_a_variable.py
@@ -0,0 +1,17 @@
+"""Author Arianna Delgado
+Created on July 18, 2020
+"""
+
+x = 123
+
+#Declares a function.
+def result():
+    x = 30 
+    print(x)
+    print(globals()['x'])
+
+
+#This asssing a function to a variable.
+y = result
+y()
+y()
