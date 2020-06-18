f249e28644928b66c1e532a27bb91f7faa85c62a
Arianna <sarozadr@gmail.com>
2020-06-18 17:16:03 -0400

    Inicial commit

---
+++ functions/keyword_arguments.py
@@ -0,0 +1,21 @@
+"""Author Arianna Delgado
+Created on July 14, 2020
+"""
+
+
+#Defines a function to calculate average.
+def average(a,b):
+    #This variables get values as we send it.
+    #The order is not important the keywords are important for teh values.
+    print(a)
+    print(b)
+    return (a+b)/2
+
+
+#Calls the function average() with passing values, 
+#The return value of the function is aggregated to variable result and pinted.
+#result = average(a = 20, b = 10)  
+
+#Switch the variables names.
+result = average(b = 20, a = 10) 
+print (result)  
