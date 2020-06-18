ef1dad315f5a19009c1ae3f1ceb85a062170be59
Arianna <sarozadr@gmail.com>
2020-06-18 18:27:58 -0400

    Initial commit

---
+++ functions/even_or_odd_lambda.py
@@ -0,0 +1,8 @@
+"""Author Arianna Delgado
+Created on July 18, 2020
+"""
+
+"""Create a Lambda that will return YES if a given number is even and NO if the given number is odd."""
+
+f = lambda x: 'Yes' if x % 2 == 0 else 'No'
+print (f(2))
