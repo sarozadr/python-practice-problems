8e2f38ce8be4a2a527f7780423ab5b1bf7cb9cd4
Arianna <sarozadr@gmail.com>
2020-06-18 18:52:48 -0400

    Initial commit

---
+++ functions/double_using_map.py
@@ -0,0 +1,17 @@
+"""Author Arianna Delgado
+Created on July 18, 2020
+"""
+
+"""Using the MAP function double the value of each element in the list"""
+
+list_num = [2,3,4,5]
+
+#Takes a lambda expression and a sequence of pararmeters.
+#Converts the map result to a list
+result = list(map(lambda x: x * 2, list_num))
+
+print(result)
+
+
+
+
