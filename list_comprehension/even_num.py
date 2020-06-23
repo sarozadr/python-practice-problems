d2d8a00debb626c5e0b2aa4e8822261f0f8d0c55
Arianna <sarozadr@gmail.com>
2020-06-23 16:20:23 -0400

    Initial commit

--- list_comprehension/even_num.py
+++ list_comprehension/even_num.py
@@ -4,5 +4,13 @@ Created on June 23, 2020
 
 """Even numbers between 1 to 20 using list comprehension."""
 
-even_list = [x % 2 == 0 for x in range(1,21)]
+'''even_list =[x for x in range(2,21,2)]
+print(even_list)'''
+
+#Works as a list comprehension: expresion, loop, condition.
+even_list = [x  for x in range(1,21) if x % 2 == 0]
 print(even_list)
+
+
+
+
