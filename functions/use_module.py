fcdd7caeaf2ea03da4182dfeff89a19d87d66e62
Arianna <sarozadr@gmail.com>
2020-06-23 14:53:01 -0400

    Initial commit

--- functions/use_module.py
+++ functions/use_module.py
@@ -2,11 +2,19 @@
 Created on June 22, 2020
 """
 
-#Import the module
+#Import the module 
 import my_math
+
+#Alias name to a module
+import my_math as ari
+
+#Imports all the function from that particular module
+#from my_math import *
 
 
 #Invokes the sum and diff functions
 print(my_math.sum(10,5))
-
 print(my_math.diff(10,5))
+
+print(ari.sum(10,5))
+print(ari.diff(10,5))
