f8faedecac4cbe4f0750d5dbcf3d6d8698cc21f2
Arianna <sarozadr@gmail.com>
2020-06-22 23:50:45 -0400

    Initial commit

---
+++ functions/custom_generator.py
@@ -0,0 +1,18 @@
+"""Author Arianna Delgado
+Created on June 22, 2020
+"""
+
+"""A Generato is a sequence of values"""
+
+#Defines a function passing two values
+def customgen(x,y):
+    while x < y:
+        #Storages all the values of x in memory and at the end it will return 
+        yield  x
+        #Increaments the values of x until it reaches y-1
+        x += 1
+
+
+result = customgen(20,30)
+
+for i in result: print(i)
