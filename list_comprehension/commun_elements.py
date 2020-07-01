aed6bec9713f01ac0e9c8a7cbaaadab6a02f586b
Arianna <sarozadr@gmail.com>
2020-07-01 16:19:33 -0400

    Initial commit

---
+++ list_comprehension/commun_elements.py
@@ -0,0 +1,10 @@
+"""Author Arianna Delgado
+Created on June 25, 2020
+"""
+
+"""Find commun elements of two list with a list comprehension"""
+
+first_list = [1,2,3,4,5]
+second_list = [5,6,7,8,9]
+
+new_list =[if first_list[i] ==  for i in range(len(first_list)) ]
