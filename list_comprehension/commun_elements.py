d84574422f16898461456ece14bce113cf0b7138
Arianna <sarozadr@gmail.com>
2020-07-01 16:32:00 -0400

    Fixed code

--- list_comprehension/commun_elements.py
+++ list_comprehension/commun_elements.py
@@ -7,4 +7,14 @@ Created on June 25, 2020
 first_list = [1,2,3,4,5]
 second_list = [5,6,7,8,9]
 
-new_list =[if first_list[i] ==  for i in range(len(first_list)) ]
+'''#Uses loop  to solve the proplem.
+for i in first_list
+    if i in second_list
+        result.append(i)'''
+
+
+
+#Uses list comprehension for solution.
+new_list =[i for i in first_list if i in second_list]
+
+print(new_list)
